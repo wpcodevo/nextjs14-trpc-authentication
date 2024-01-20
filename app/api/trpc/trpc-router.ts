595fa26a109c32212a4d35115c30360a28328801
Edem <contact@codevoweb.com>
2024-01-20 12:56:45 +0000

    setup trpc client and server

---
+++ app/api/trpc/trpc-router.ts
@@ -0,0 +1,23 @@
+import { t } from '@/utils/trpc-server';
+import { createServerSideHelpers } from '@trpc/react-query/server';
+import SuperJSON from 'superjson';
+
+const healthCheckerRouter = t.router({
+  healthchecker: t.procedure.query(({ ctx }) => {
+    return {
+      status: 'success',
+      message: 'Welcome to trpc with Next.js 14 and React Query',
+    };
+  }),
+});
+
+export const appRouter = t.mergeRouters(healthCheckerRouter);
+
+export const createSSRHelper = () =>
+  createServerSideHelpers({
+    router: appRouter,
+    transformer: SuperJSON,
+    ctx: () => {},
+  });
+
+export type AppRouter = typeof appRouter;
